7df0f3d46ff3348d26f3bf0e17989b7d3e263980
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-10-13 00:16:07 -0500

    Add .csv and .txt parsing to graph constructor, print graph method

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,8 +1,54 @@
 #include "graph.h"
+#include <fstream>
+#include <sstream>
+using std::ifstream;
+using std::stringstream;
+using std::cout;
+using std::endl;
 
-Graph::Graph(int size) : adjList(vector<vector<int>>(size)){}
+Graph::Graph(const string& csv, const string& adjacencies){
+    parseCSV(csv);
+    parseAdjacencies(adjacencies);
+}
 
 void Graph::addEdge(int u, int v){
     adjList[u].push_back(v);
     adjList[v].push_back(u);
+}
+
+void Graph::parseCSV(const string& fileName){
+    ifstream file(fileName);
+    string line;
+    int i = 0;
+    if(!file.is_open()){return;}
+    while(getline(file, line)){
+        stringstream ss(line);
+        string city;
+        if(getline(ss, city, ',')){
+            cityMap[city]=i++;
+            indexToCity.push_back(city);
+            adjList.push_back({});
+        }
+    }
+    file.close();
+}
+
+void Graph::parseAdjacencies(const string& fileName){
+    ifstream file(fileName);
+    string line;
+    if(!file.is_open()){return;}
+    while(getline(file, line)){
+        stringstream ss(line);
+        string city1, city2;
+        if(ss >> city1 >> city2){addEdge(cityMap[city1],cityMap[city2]);}
+    }
+    file.close();
+}
+
+void Graph::printGraph(){
+    for(int i = 0; i < adjList.size(); i++){
+        cout << indexToCity[i] << " -> ";
+        for(int neighbor : adjList[i]){cout << indexToCity[neighbor] << " ";}
+        cout << endl;
+    }
 }
--- src/graph.h
+++ src/graph.h
@@ -2,14 +2,24 @@
 #define GRAPH_H
 
 #include <vector>
+#include <string>
+#include <unordered_map>
+#include <iostream>
 using std::vector;
+using std::string;
+using std::unordered_map;
 
 class Graph {
 private:
     vector<vector<int>> adjList;
-public:
-    Graph(int size);
+    unordered_map<string,int> cityMap;
     void addEdge(int src, int dest);
+    void parseCSV(const string& fileName);
+    void parseAdjacencies(const string& fileName);
+    vector<string> indexToCity;
+public:
+    Graph(const string& csv, const string& adjacencies);
+    void printGraph();
 };
 
 #endif GRAPH_H
