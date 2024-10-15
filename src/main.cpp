c83008f30618fc6b396e81f4790f52b3e6a08d30
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-10-14 22:32:04 -0500

    Move csv/txt to input folder, clean up test code

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,25 @@
 #include "iddfs.h"
 
 int main(){
-    string csvFile = "src/coordinates.csv";
-    string adjFile = "src/Adjacencies.txt";
+    string csvFile = "input/coordinates.csv";
+    string adjFile = "input/Adjacencies.txt";
 
     Graph graph(csvFile, adjFile);
 
-    pair<vector<int>,int> bfsResults = bfs(graph.getAdjList(), graph.getCityIndex("Augusta"), graph.getCityIndex("Cheney"));
+    const vector<vector<int>> adjList = graph.getAdjList();
+    int cityA = graph.getCityIndex("Augusta");
+    int cityB = graph.getCityIndex("Cheney");
+
+    pair<vector<int>,int> bfsResults = bfs(adjList, cityA, cityB);
     graph.printRoute(bfsResults.first);
     cout << "Breadth-first execution time: " << bfsResults.second << " microseconds" << endl;
 
-    pair<vector<int>,int> dfsResults = dfs(graph.getAdjList(), graph.getCityIndex("Augusta"), graph.getCityIndex("Cheney"));
+    pair<vector<int>,int> dfsResults = dfs(adjList, cityA, cityB);
     graph.printRoute(dfsResults.first);
     cout << "Depth-first execution time: " << dfsResults.second << " microseconds" << endl;
 
     int maxDepth = 10;
-    pair<vector<int>,int> iddfsResults = iddfs(graph.getAdjList(), graph.getCityIndex("Augusta"), graph.getCityIndex("Cheney"), maxDepth);
+    pair<vector<int>,int> iddfsResults = iddfs(adjList, cityA, cityB, maxDepth);
     graph.printRoute(iddfsResults.first);
     cout << "IDDFS execution time (max depth := " << maxDepth << " ): " << iddfsResults.second << " microseconds" << endl;
 }
